f3af6bdd959666ba8e76ba45933fa16baab00ea7
Rosie Choi <rosiechoimanso@gmail.com>
2020-12-15 22:13:21 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,7 +3,6 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  include Dance
 
   attr_accessor :name
 
@@ -11,7 +10,7 @@ class Dancer
     @name = name
   end
 
-  extend MetaDancing
+  
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
